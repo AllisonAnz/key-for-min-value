0c35d9fdddf8be7b4c44e54e044168c82d67bf5b
AllisonAnz <AllisonAnzalone@gmail.com>
2020-11-20 20:31:26 -0600

    commit tree f6cd31dd2f8a8f0288f93490094a3c1ed6824e76

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    pets = {ally: 9,
+            marian: 8,
+            willow: 10
+}
+lowest_key = nil
+lowest_value = nil
+name_hash.each do |pets, age|
+    if lowest_value == nil || age < lowest_value
+        lowest_value = age
+        lowest_key = pets
+    end
+end
+    lowest_key
 end
